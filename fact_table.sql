def36a32977748341f966900d1b86043c324e43c
Zehao Xiao <100718312+Zehao-x@users.noreply.github.com>
2026-02-05 15:01:11 -0500

    update sql code of create and merge fact table

--- fact_table.sql
+++ fact_table.sql
@@ -6,7 +6,7 @@ create table if not exists AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
     -- Foreign keys
     DATE_KEY        DATE NOT NULL,
     SYMBOL           VARCHAR(16) NOT NULL,
-    company_key       VARCHAR(16) not null,
+    COMPANY_KEY       VARCHAR(16) not null,
 
     -- Measures
     OPEN_PRICE           NUMBER(18,6),
@@ -20,7 +20,7 @@ create table if not exists AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
     MA_7            NUMBER(18,6),
     MA_30           NUMBER(18,6),
     DAILY_RETURN    NUMBER(18,6),
-    DAILY_CHANGE    NUMBER(18,6)
+    DAILY_CHANGE    NUMBER(18,6),
 
     -- Metadata
     LOAD_TS              TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP,
@@ -54,8 +54,8 @@ SELECT
     sh.HIGH,
     sh.LOW,
     sh.CLOSE,
-    sh.ADJCLOSE,
     sh.VOLUME,
+    sh.ADJCLOSE,
     sh.VOLAVG,
     sh.CHANGES,
     sh.MA_7,
@@ -66,7 +66,7 @@ FROM STG_STOCK_HISTORY sh
 JOIN DIM_DATE dd
   ON sh.DATE = dd.FULL_DATE
 JOIN DIM_SYMBOL ds
-  ON sh.SYMBOL = ds.SYMBOL
+  ON sh.SYMBOL = ds.SYMBOL_KEY
 JOIN DIM_COMPANY dc
   ON sh.SYMBOL = dc.SYMBOL
 
@@ -84,8 +84,8 @@ USING (
         sh.HIGH,
         sh.LOW,
         sh.CLOSE,
-        sh.ADJCLOSE,
         sh.VOLUME,
+		sh.ADJCLOSE,
         sh.VOLAVG,
         sh.CHANGES,
         sh.MA_7,
@@ -96,7 +96,7 @@ USING (
     JOIN DIM_DATE dd
       ON sh.DATE = dd.FULL_DATE
     JOIN DIM_SYMBOL ds
-      ON sh.SYMBOL = ds.SYMBOL
+      ON sh.SYMBOL = ds.SYMBOL_KEY
     JOIN DIM_COMPANY dc
       ON sh.SYMBOL = dc.SYMBOL
 ) src
@@ -105,12 +105,12 @@ AND tgt.SYMBOL_KEY  = src.SYMBOL_KEY
 AND tgt.COMPANY_KEY = src.COMPANY_KEY
 
 WHEN MATCHED THEN UPDATE SET
-    tgt.OPEN_PRICE   = src.OPEN,
-    tgt.HIGH_PRICE   = src.HIGH,
-    tgt.LOW_PRICE    = src.LOW,
-    tgt.CLOSE_PRICE  = src.CLOSE,
-    tgt.ADJ_CLOSE    = src.ADJCLOSE,
+    tgt.OPEN_PRICE   = src.OPEN_PRICE,
+    tgt.HIGH_PRICE   = src.HIGH_PRICE,
+    tgt.LOW_PRICE    = src.LOW_PRICE,
+    tgt.CLOSE_PRICE  = src.CLOSE_PRICE,
     tgt.VOLUME       = src.VOLUME,
+	tgt.ADJ_CLOSE    = src.ADJCLOSE_PRICE,
     tgt.VOLAVG       = src.VOLAVG,
     tgt.CHANGES      = src.CHANGES,
     tgt.MA_7         = src.MA_7,
@@ -127,8 +127,8 @@ WHEN NOT MATCHED THEN INSERT (
     HIGH_PRICE,
     LOW_PRICE,
     CLOSE_PRICE,
+	VOLUME,
     ADJ_CLOSE,
-    VOLUME,
     VOLAVG,
     CHANGES,
     MA_7,
@@ -140,12 +140,12 @@ VALUES (
     src.DATE_KEY,
     src.SYMBOL_KEY,
     src.COMPANY_KEY,
-    src.OPEN,
-    src.HIGH,
-    src.LOW,
-    src.CLOSE,
-    src.ADJCLOSE,
-    src.VOLUME,
+    src.OPEN_PRICE,
+    src.HIGH_PRICE,
+    src.LOW_PRICE,
+    src.CLOSE_PRICE,
+    src.VOLUME_PRICE,
+	src.ADJCLOSE_PRICE,
     src.VOLAVG,
     src.CHANGES,
     src.MA_7,

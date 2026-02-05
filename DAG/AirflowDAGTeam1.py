2755bfec6872f2edb01f686d222f576fa69e1017
Huiyu Song <huiyus1999@gmail.com>
2026-02-05 17:09:28 -0500

    Update AirflowDAGTeam1.py

--- DAG/AirflowDAGTeam1.py
+++ DAG/AirflowDAGTeam1.py
@@ -1,3 +1,4 @@
+
 from __future__ import annotations
 
 from datetime import datetime, timedelta
@@ -22,47 +23,44 @@ with DAG(
     default_args=default_args,
     tags=["project1", "snowflake", "stock", "team1",'etl','dwh'],
 ) as dag:
-    load_dims = SnowflakeOperator(
-        task_id="load_dims",
-        snowflake_conn_id="jan_airflow_snowflake",
-        sql="""
-    """,
-    )
+
 
     load_fact = SnowflakeOperator(
         task_id="load_fact",
         snowflake_conn_id="jan_airflow_snowflake",
-        sql="""create table if not exists AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
-    PK_STOCK_HISTORY_ID NUMBER AUTOINCREMENT,
+        sql="""--create fact table
+CREATE TABLE IF NOT EXISTS AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
+    STOCK_DAILY_KEY NUMBER AUTOINCREMENT,
 
     -- Foreign keys
     DATE_KEY        DATE NOT NULL,
-    SYMBOL           VARCHAR(16) NOT NULL,
-    company_key       VARCHAR(16) not null,
+    COMPANY_KEY     VARCHAR(16) NOT NULL,
 
     -- Measures
-    OPEN_PRICE           NUMBER(18,6),
-    HIGH_PRICE           NUMBER(18,6),
-    LOW_PRICE            NUMBER(18,6),
-    CLOSE_PRICE          NUMBER(18,6),
-    VOLUME               NUMBER(18,0),
-    ADJCLOSE_PRICE      NUMBER(18,6),
+    OPEN_PRICE      NUMBER(18,6),
+    HIGH_PRICE      NUMBER(18,6),
+    LOW_PRICE       NUMBER(18,6),
+    CLOSE_PRICE     NUMBER(18,6),
+    VOLUME          NUMBER(18,0),
+	ADJCLOSE_PRICE  NUMBER(18,6),
+
     VOLAVG          NUMBER(18,6),
     CHANGES         NUMBER(18,6),
     MA_7            NUMBER(18,6),
     MA_30           NUMBER(18,6),
     DAILY_RETURN    NUMBER(18,6),
-    DAILY_CHANGE    NUMBER(18,6)
+    DAILY_CHANGE    NUMBER(18,6),
 
     -- Metadata
-    LOAD_TS              TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP,
+    LOAD_TS         TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT PK_fact_1 PRIMARY KEY (PK_STOCK_HISTORY_ID)
+    CONSTRAINT PK_FACT_STOCK_HISTORY_1
+        PRIMARY KEY (STOCK_DAILY_KEY)
 );
 
-insert into AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
+--insert data
+INSERT INTO AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
     DATE_KEY,
-    SYMBOL,
     COMPANY_KEY,
     OPEN_PRICE,
     HIGH_PRICE,
@@ -75,19 +73,17 @@ insert into AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 (
     MA_7,
     MA_30,
     DAILY_RETURN,
-    DAILY_CHANGE,
-    
+    DAILY_CHANGE
 )
 SELECT
     dd.DATE_KEY,
-    ds.SYMBOL_KEY,
     dc.COMPANY_KEY,
     sh.OPEN,
     sh.HIGH,
     sh.LOW,
     sh.CLOSE,
-    sh.ADJCLOSE,
     sh.VOLUME,
+	sh.ADJCLOSE,
     sh.VOLAVG,
     sh.CHANGES,
     sh.MA_7,
@@ -97,70 +93,96 @@ SELECT
 FROM STG_STOCK_HISTORY sh
 JOIN DIM_DATE dd
   ON sh.DATE = dd.FULL_DATE
-JOIN DIM_SYMBOL ds
-  ON sh.SYMBOL = ds.SYMBOL
 JOIN DIM_COMPANY dc
-  ON sh.SYMBOL = dc.SYMBOL
+  ON sh.SYMBOL = dc.SYMBOL;
 
 
-
---merge fact table
-
-MERGE INTO AIRFLOW0105.DEV.FACT_STOCK_DAILY_1 tgt
+--merge fact table for new data coming in
+MERGE INTO AIRFLOW0105.DEV.FACT_STOCK_HISTORY_1 tgt
 USING (
-    SELECT
-        dd.DATE_KEY,
-        ds.SYMBOL_KEY,
-        dc.COMPANY_KEY,
-        sh.OPEN,
-        sh.HIGH,
-        sh.LOW,
-        sh.CLOSE,
-        sh.ADJCLOSE,
-        sh.VOLUME,
-        sh.VOLAVG,
-        sh.CHANGES,
-        sh.MA_7,
-        sh.MA_30,
-        sh.DAILY_RETURN,
-        sh.DAILY_CHANGE
+   SELECT
+        dd.DATE_KEY                    AS DATE_KEY,
+        dc.COMPANY_KEY                 AS COMPANY_KEY,
+
+        sh.OPEN                        AS OPEN_PRICE,
+        sh.HIGH                        AS HIGH_PRICE,
+        sh.LOW                         AS LOW_PRICE,
+		sh.ADJCLOSE                    AS ADJCLOSE_PRICE,
+        sh.CLOSE                       AS CLOSE_PRICE,
+        sh.VOLUME                      AS VOLUME,
+
+        /* ===== Derived metrics ===== */
+
+        -- Daily absolute change
+        sh.CLOSE - sh.OPEN             AS DAILY_CHANGE,
+
+        -- Daily return (%)
+        CASE 
+            WHEN sh.OPEN <> 0 
+            THEN (sh.CLOSE - sh.OPEN) / sh.OPEN 
+            ELSE NULL 
+        END                            AS DAILY_RETURN,
+
+        -- Change vs previous close
+        sh.CLOSE 
+        - LAG(sh.CLOSE) OVER (
+            PARTITION BY dc.COMPANY_KEY
+            ORDER BY dd.FULL_DATE
+          )                             AS CHANGES,
+
+        -- 7-day moving average (close)
+        AVG(sh.CLOSE) OVER (
+            PARTITION BY dc.COMPANY_KEY
+            ORDER BY dd.FULL_DATE
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        )                               AS MA_7,
+
+        -- 30-day moving average (close)
+        AVG(sh.CLOSE) OVER (
+            PARTITION BY dc.COMPANY_KEY
+            ORDER BY dd.FULL_DATE
+            ROWS BETWEEN 29 PRECEDING AND CURRENT ROW
+        )                               AS MA_30,
+
+        -- 7-day rolling average volume
+        AVG(sh.VOLUME) OVER (
+            PARTITION BY dc.COMPANY_KEY
+            ORDER BY dd.FULL_DATE
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        )                               AS VOLAVG
     FROM STG_STOCK_HISTORY sh
     JOIN DIM_DATE dd
       ON sh.DATE = dd.FULL_DATE
-    JOIN DIM_SYMBOL ds
-      ON sh.SYMBOL = ds.SYMBOL
     JOIN DIM_COMPANY dc
       ON sh.SYMBOL = dc.SYMBOL
 ) src
 ON  tgt.DATE_KEY    = src.DATE_KEY
-AND tgt.SYMBOL_KEY  = src.SYMBOL_KEY
 AND tgt.COMPANY_KEY = src.COMPANY_KEY
 
 WHEN MATCHED THEN UPDATE SET
-    tgt.OPEN_PRICE   = src.OPEN,
-    tgt.HIGH_PRICE   = src.HIGH,
-    tgt.LOW_PRICE    = src.LOW,
-    tgt.CLOSE_PRICE  = src.CLOSE,
-    tgt.ADJ_CLOSE    = src.ADJCLOSE,
-    tgt.VOLUME       = src.VOLUME,
-    tgt.VOLAVG       = src.VOLAVG,
-    tgt.CHANGES      = src.CHANGES,
-    tgt.MA_7         = src.MA_7,
-    tgt.MA_30        = src.MA_30,
-    tgt.DAILY_RETURN = src.DAILY_RETURN,
-    tgt.DAILY_CHANGE = src.DAILY_CHANGE,
-    tgt.LOAD_TS      = CURRENT_TIMESTAMP
+    tgt.OPEN_PRICE      = src.OPEN,
+    tgt.HIGH_PRICE      = src.HIGH,
+    tgt.LOW_PRICE       = src.LOW,
+    tgt.CLOSE_PRICE     = src.CLOSE,
+    tgt.VOLUME          = src.VOLUME,
+	tgt.ADJCLOSE_PRICE  = src.ADJCLOSE,
+    tgt.VOLAVG          = src.VOLAVG,
+    tgt.CHANGES         = src.CHANGES,
+    tgt.MA_7            = src.MA_7,
+    tgt.MA_30           = src.MA_30,
+    tgt.DAILY_RETURN    = src.DAILY_RETURN,
+    tgt.DAILY_CHANGE    = src.DAILY_CHANGE,
+    tgt.LOAD_TS         = CURRENT_TIMESTAMP
 
 WHEN NOT MATCHED THEN INSERT (
     DATE_KEY,
-    SYMBOL_KEY,
     COMPANY_KEY,
     OPEN_PRICE,
     HIGH_PRICE,
     LOW_PRICE,
     CLOSE_PRICE,
-    ADJ_CLOSE,
-    VOLUME,
+	VOLUME,
+    ADJCLOSE_PRICE,
     VOLAVG,
     CHANGES,
     MA_7,
@@ -170,21 +192,23 @@ WHEN NOT MATCHED THEN INSERT (
 )
 VALUES (
     src.DATE_KEY,
-    src.SYMBOL_KEY,
     src.COMPANY_KEY,
     src.OPEN,
     src.HIGH,
     src.LOW,
     src.CLOSE,
+	src.VOLUME,
     src.ADJCLOSE,
-    src.VOLUME,
     src.VOLAVG,
     src.CHANGES,
     src.MA_7,
     src.MA_30,
     src.DAILY_RETURN,
     src.DAILY_CHANGE
-)""",
+);
+
+
+  """
     )
 
     validate = SnowflakeOperator(
@@ -238,4 +262,4 @@ ON cte1.exchange = cte2.exchange;
 -- 23 same symbol between two table, which means they are same""",
     )
 
-    load_dims >> load_fact >> validate
+     load_fact >> validate
